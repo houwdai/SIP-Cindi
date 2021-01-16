5ef5591865f31f348c3e01d9cd433f9fbcb59177
houwdai <cinditrifitikasari_1313618025@mhs.unj.ac.id>
2021-01-16 15:08:32 +0700

    full

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -37,7 +37,6 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-
         ],
 
         'api' => [
@@ -63,6 +62,5 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'role' => \Illuminate\Routing\Middleware\RoleMiddleware::class,
     ];
 }
